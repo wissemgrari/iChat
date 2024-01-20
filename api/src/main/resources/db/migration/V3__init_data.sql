9118c34e73d465cd1e1482b58cf37f05713bbaa3
wissemgrari <wissemgrari@outlook.com>
2024-01-20 21:19:01 +0100

    fix migration script

--- api/src/main/resources/db/migration/V3__init_data.sql
+++ api/src/main/resources/db/migration/V3__init_data.sql
@@ -1,35 +1,37 @@
 -- Insert data into the users table
+
 INSERT INTO users (id, first_name, last_name, email, password)
 VALUES (nextval('user_sequence'), 'Wissem', 'Grari', 'grariwissem@gmail.com',
         '$2a$10$.NyruY7apwsSBp6YodO9AO4Nlchu1iLkKcT5QrKIoVuDknTHuvaLC');
-
 INSERT INTO users (id, first_name, last_name, email, password)
 VALUES (nextval('user_sequence'), 'John', 'Doe', 'johndoe@gmail.com',
         '$2a$10$jlLkw3aBC/ycSPKM6.FINutG749PoVjVqz16PqcCD9u6RVaEPcwGK');
-
 INSERT INTO users (id, first_name, last_name, email, password)
 VALUES (nextval('user_sequence'), 'Jean', 'Doe', 'jeandoe@gmail.com',
         '$2a$10$SV0XckOh0A.syt0ycUYrKe3v.FuhleZJJ6El8sI7iH.QdEymFw0um');
-
 INSERT INTO users (id, first_name, last_name, email, password)
 VALUES (nextval('user_sequence'), 'Sarah', 'Smith', 'sarahsmith@gmail.com',
         '$2a$10$pF1pxa3J0Z98gNkCBZMW5.uDW55hQmy27cQwHm2/ZRaGWEVf/gO3O');
 
-INSERT INTO chats (id, created_at)
-VALUES (1, '2023-09-20 13:02:38.252443');
+-- ************************************************************************************************************* --
 
 INSERT INTO chats (id, created_at)
-VALUES (2, '2024-01-24 20:31:38.132443');
+VALUES (nextval('chat_sequence'), '2023-09-20 13:02:38.252443');
 
 INSERT INTO user_chat (chat_id, user_id, participant_id)
 VALUES (1, 1, 2);
 
+-- ************************************************************************************************************* --
+
+INSERT INTO chats (id, created_at)
+VALUES (nextval('chat_sequence'), '2024-01-24 20:31:38.132443');
+
 INSERT INTO user_chat (chat_id, user_id, participant_id)
 VALUES (2, 2, 3);
 
-
+-- ************************************************************************************************************* --
 INSERT INTO messages (id, chat_id, user_id, content, created_at, status)
-VALUES (nextval('message_sequence'), nextval('chat_sequence'), 1, 'Hello there', '2023-09-20 13:02:48.433500',
+VALUES (nextval('message_sequence'), currval('chat_sequence'), 1, 'Hello there', '2023-09-20 13:02:48.433500',
         'DELIVERED');
 
 INSERT INTO messages (id, chat_id, user_id, content, created_at, status)
