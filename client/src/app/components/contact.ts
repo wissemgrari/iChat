8658e0c7d3604bba01e924341d01bbd0cfda586b
wissemgrari <wissemgrari@outlook.com>
2024-02-03 20:15:30 +0100

    Add ContactComponent and update modal design

---
+++ client/src/app/components/contact.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { User } from 'types/global-types';
+
+@Component({
+  selector: 'contact',
+  template: `
+    <div
+      class="flex items-center gap-3 py-3 px-2 rounded-lg cursor-pointer hover:bg-grey/10 transition-all duration-200"
+    >
+      <avatar [user]="user" [style]="'square'" />
+      <div class="flex flex-col gap-1 text-white">
+        <p class="capitalize text-sm">{{ user.firstName }} {{ user.lastName }}</p>
+        <p class="text-xs text-white/70">{{ user.email }}</p>
+      </div>
+    </div>
+  `,
+})
+export class ContactComponent {
+  user: User = {
+    id: 11,
+    email: 'johndoe@gmail.com',
+    firstName: 'john',
+    lastName: 'doe',
+  };
+}
--- client/src/app/components/modal/modal.ts
+++ client/src/app/components/modal/modal.ts
@@ -4,22 +4,34 @@ import { ModalService } from './modal.service';
 @Component({
   selector: 'modal',
   template: `
-    <div>
+    <div class="flex items-center justify-between">
+      <p class="text-white text-base uppercase">Contacts</p>
       <img
         (click)="hideModal()"
         src="/assets/close.svg"
-        class="w-7 h-7 select-none rounded-full p-1 cursor-pointer hover:bg-grey/20 ml-auto opacity-60"
+        class="w-7 h-7 select-none rounded-full p-1 cursor-pointer hover:bg-grey/20"
       />
-      <div
-        class="flex gap-x-2 mt-5 border border-grey px-4 py-2 rounded-full items-center"
-      >
-        <img src="/assets/search.svg" alt="" class="w-5 opacity-60" />
-        <input
-          type="search"
-          placeholder="find contact"
-          class="w-full p-1 bg-transparent outline-none placeholder:text-sm placeholder:text-white/60 text-white"
-        />
-      </div>
+    </div>
+
+    <div
+      class="flex gap-x-2 mt-5 px-4 py-2 rounded-full items-center border-2 border-grey/20 focus-within:border-grey/50 transition-all duration-300"
+    >
+      <img src="/assets/search.svg" alt="" class="w-5 opacity-60" />
+      <input
+        type="search"
+        placeholder="Search"
+        class="w-full p-1 bg-transparent outline-none text-sm placeholder:text-sm placeholder:text-white/60 text-white"
+      />
+    </div>
+
+    <div class="mt-5 max-h-72 overflow-y-auto">
+      <contact></contact>
+      <contact></contact>
+      <contact></contact>
+      <contact></contact>
+      <contact></contact>
+      <contact></contact>
+      <contact></contact>
     </div>
   `,
 })
